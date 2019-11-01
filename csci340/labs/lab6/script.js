dca76327a800fbf2a75e5ab17b2d7ff32b8b9a00
ericpinter <ericpinter99@gmail.com>
2019-10-31 22:05:04 -0500

    Making images scale correctly

--- csci340/labs/lab6/script.js
+++ csci340/labs/lab6/script.js
@@ -33,18 +33,22 @@ const getPicture = () =>{
     jsonpCallback: "parseQuote",
     url: "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true",
     success: function(results) {
+
+            $('#size_controller').removeAttr("height");
+            $('#size_controller').removeAttr("width");
+            $('#size_controller').attr("src",results[0]);
+            let h = $('#size_controller').height();
+            console.log('h=',h);
+            if (h/500.0 > 1) {
+              console.log("Scaled the image for you!");
+              let r = h/500.0;
+              console.log(r);
+              $('#size_controller').attr("height","500px");
+              //let w = $('#size_controller').width();
+              //$('#size_controller').attr("width",w/r+"px");
+            }
       $('#picture').css("background-image", "url("+results[0]+")");
-      $('#size_controller').removeAttr("height");
-      $('#size_controller').attr("src",results[0]);
-      let h = $('#size_controller').height();
-      console.log('h=',h);
-      if (h/500.0 > 1) {
-        console.log("Scaled the image for you!");
-        let r = h/500.0;
-        $('#size_controller').attr("height",500);
-        let w = $('#size_controller').attr("width");
-        $('#size_controller').attr("width",w/r);
-      }
+
     },
     error: function(xhr,status,error) {
       console.log(error);
